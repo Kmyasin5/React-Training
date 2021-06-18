08382c6b29e51af2c253b9b8966f6cc724d39463
Kmyasin5 <79813819+Kmyasin5@users.noreply.github.com>
2021-06-18 16:25:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ Greet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// function Greet() {
+//     return <h1>Asalamu Alaikum Yasin</h1>
+// }
+
+const Greet = () => <h1>Yasin</h1>
+
+export default Greet
